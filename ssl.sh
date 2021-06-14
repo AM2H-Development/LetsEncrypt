4f5acac2cd40e254be1f3e4c508207b54e2eb4f4
jq7140 <am@clicca.de>
2021-06-14 20:48:14 +0200

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker run -t --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 -p 443:443 \
-certbot/certbot certonly --standalone -d $1 --keep-until-expiring
+certbot/certbot certonly --standalone -d $1 --keep-until-expiring > /var/log/letsencrypt.log
 
 cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > /etc/cockpit/ws-certs.d/1-cert.cert
 

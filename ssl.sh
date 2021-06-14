48b6469041f783e90ced4cd00cfe5da93aafaf53
jq7140 <am@clicca.de>
2021-06-14 20:47:10 +0200

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 -p 443:443 \
+docker run -t --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 -p 443:443 \
 certbot/certbot certonly --standalone -d $1 --keep-until-expiring
 
 cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > /etc/cockpit/ws-certs.d/1-cert.cert
+
+cp /etc/letsencrypt/live/$1/privkey.pem /var/certs/
+cp /etc/letsencrypt/live/$1/fullchain.pem /var/certs/
+cp /etc/letsencrypt/live/$1/cert.pem /var/certs/
+
+docker cp /var/certs/privkey.pem influx_influxdb_reverse:/etc/nginx/
+docker cp /var/certs/fullchain.pem influx_influxdb_reverse:/etc/nginx/
+
+docker cp /var/certs/privkey.pem influx_grafana_reverse:/etc/nginx/
+docker cp /var/certs/fullchain.pem influx_grafana_reverse:/etc/nginx/
+
 systemctl restart cockpit.service
-
-
-cp /etc/letsencrypt/live/$1/privkey.pem /tmp/
-cp /etc/letsencrypt/live/$1/fullchain.pem /tmp/
-docker cp /tmp/privkey.pem nginx-reverse_nginx_1:/etc/nginx/
-docker cp /tmp/fullchain.pem nginx-reverse_nginx_1:/etc/nginx/
-rm /tmp/privkey.pem
-rm /tmp/fullchain.pem
+docker restart influx_influxdb_reverse
+docker restart influx_grafana_reverse
